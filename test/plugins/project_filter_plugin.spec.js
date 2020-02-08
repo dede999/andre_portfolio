417cb8dc584039af84bc9d34dd1963fbafd2976e
André Luizs <andreluizsa00@gmail.com>
2020-02-08 01:36:46 -0300

    Corrigindo testes

--- test/plugins/project_filter_plugin.spec.js
+++ test/plugins/project_filter_plugin.spec.js
@@ -5,7 +5,7 @@ describe('#esta_no_array_de_filtros', () => {
     test('retorna true quando o elemento é encontrado no array', () => {
         expect(funcs.esta_no_array_de_filtros('eu', ['ele', 'eu', 'ela'])).toBeTruthy()
     }),
-    test('retorna true quando o elemento é encontrado no array', () => {
+    test('retorna false quando o elemento é encontrado no array', () => {
         expect(funcs.esta_no_array_de_filtros('nos', ['ele', 'eu', 'ela'])).toBeFalsy()
     })
 });
@@ -14,7 +14,7 @@ describe('#projetos_escolhidos', () => {
     test('retorna true se o projeto utiliza js', () => {
         expect(funcs.projetos_escolhidos(projects[8], ['js'])).toBeTruthy()
     }),
-    test('retorna true quando o elemento é encontrado no array', () => {
+    test('retorna false se o projeto não utiliza js', () => {
         expect(funcs.projetos_escolhidos(projects[7], ['js'])).toBeFalsy()
     }),
     test('permite filtrar os n projetos que usam awk e/ou latex', () => {
